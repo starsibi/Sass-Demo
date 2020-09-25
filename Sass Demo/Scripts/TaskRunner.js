b87570a279401d3ae630676819f5829987c1b840
starsibi <starsibi@gmail.com>
2020-09-25 12:00:15 +0530

    Add project files.

---
+++ Sass Demo/Scripts/TaskRunner.js
@@ -0,0 +1,12 @@
+﻿var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+gulp.task("build-css", function () {
+    return gulp
+        .src('./Content/Styles/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./Content/Styles'));
+
+});
+
+gulp.task('default', ['build-css']);
